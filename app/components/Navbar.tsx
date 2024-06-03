2aa59d42e27b2d49a8342a7f3ad7d9494ee9f3ae
Brian Smiley <briantsmiley42@gmail.com>
2024-06-03 14:01:37 -0400

    restructures <a>/<li> nesting

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,17 +3,18 @@ export default function Navbar() {
     <header className="h-12 bg-fuchsia-500">
       <nav className="h-full">
         <ul className="font-semibold text-blue-200 underline flex justify-start h-full">
-          <a href="/">
-            <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1">Home</li></a>
-          <a href="/about">
-            <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1">About</li></a>
-          <a href="https://briansmiley.github.io/p5">
-            <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1">Portfolio</li></a>
-          <a href="https://github.com/briansmiley">
-            <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1">Github</li></a>
-          <a href="https://www.linkedin.com/in/brian-smiley-1289a6121/">
-            <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1">LinkedIn</li></a>
+          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="/">
+            Home</a></li>
+          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="/about">
+            About</a></li>
+          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="https://briansmiley.github.io/p5">
+            Portfolio</a></li>
+          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="https://github.com/briansmiley">
+            Github</a></li>
+          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="https://www.linkedin.com/in/brian-smiley-1289a6121/">
+            LinkedIn</a></li>
         </ul>
       </nav>
     </header>)
     } 
+
