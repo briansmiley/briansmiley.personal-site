7dac98b86ecf2035f0251bd57f5b64ad2c42b9bd
Brian Smiley <briantsmiley42@gmail.com>
2024-06-03 14:07:47 -0400

    changes <a>s to <Link>s

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,18 +1,21 @@
+import { Link } from "@remix-run/react";
+
+
 export default function Navbar() {
     return(
     <header className="h-12 bg-fuchsia-500">
       <nav className="h-full">
         <ul className="font-semibold text-blue-200 underline flex justify-start h-full">
-          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="/">
-            Home</a></li>
-          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="/about">
-            About</a></li>
-          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="https://briansmiley.github.io/p5">
-            Portfolio</a></li>
-          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="https://github.com/briansmiley">
-            Github</a></li>
-          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="https://www.linkedin.com/in/brian-smiley-1289a6121/">
-            LinkedIn</a></li>
+          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><Link to="/">
+            Home</Link></li>
+          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><Link to="/Linkbout">
+            About</Link></li>
+          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><Link to="https://briansmiley.github.io/p5">
+            Portfolio</Link></li>
+          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><Link to="https://github.com/briansmiley">
+            Github</Link></li>
+          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><Link to="https://www.linkedin.com/in/brian-smiley-1289a6121/">
+            LinkedIn</Link></li>
         </ul>
       </nav>
     </header>)
