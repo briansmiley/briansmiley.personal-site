77c9b996567cab47cf7a2770ff8b2f635c96d807
Brian Smiley <briantsmiley42@gmail.com>
2024-06-03 11:32:17 -0400

    init and barebones

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,52 @@
 import { Link } from "@remix-run/react";
 
-
+type NavBarItemProps = {
+  label: string,
+  url: string,
+  key: string,
+  internal: boolean
+}
+function NavBarLink({label, url, key, internal = true}: NavBarItemProps) {
+  return (
+    <li key={key} className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1">
+      
+      {internal ? <Link to={url}>{label}</Link> : <a href={url}></a>}
+      </li>
+  )
+}
+const links = [
+  {
+    label: "Home",
+    url: "/",
+    internal: true
+  },
+  {
+    label: "About",
+    url: "/about",
+    internal: true
+  },
+  {
+    label: "Portfolio",
+    url: "/projects",
+    internal: true
+  },
+  {
+    label: "Github",
+    url: "https://github.com/briansmiley",
+    internal: true
+  },
+  {
+    label: "LinkedIn",
+    url: "https://www.linkedin.com/in/brian-smiley-1289a6121/",
+    internal: true
+  },
+]
 export default function Navbar() {
     return(
     <header className="h-12 bg-fuchsia-500">
       <nav className="h-full">
         <ul className="font-semibold text-blue-200 underline flex justify-start h-full">
-          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><Link to="/">
-            Home</Link></li>
-          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><Link to="/about">
-            About</Link></li>
-          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><Link to="/projects">
-            Portfolio</Link></li>
-          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="https://github.com/briansmiley" rel="noopener noreferrer">
-            Github</a></li>
-          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="https://www.linkedin.com/in/brian-smiley-1289a6121/" rel="noopener noreferrer">
-            LinkedIn</a></li> 
+          {links.map((link, idx) => NavBarLink(Object.assign(link, {key: String(idx)})))}
         </ul>
       </nav>
     </header>)
