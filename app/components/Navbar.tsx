31e661145092175e1b04dc486dd09c9b88cc9814
Brian Smiley <briantsmiley42@gmail.com>
2024-06-03 14:16:04 -0400

    fixes external Links to a href

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,14 +8,14 @@ export default function Navbar() {
         <ul className="font-semibold text-blue-200 underline flex justify-start h-full">
           <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><Link to="/">
             Home</Link></li>
-          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><Link to="/bout">
+          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><Link to="/about">
             About</Link></li>
-          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><Link to="https://briansmiley.github.io/p5">
-            Portfolio</Link></li>
-          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><Link to="https://github.com/briansmiley">
-            Github</Link></li>
-          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><Link to="https://www.linkedin.com/in/brian-smiley-1289a6121/">
-            LinkedIn</Link></li>
+          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="https://briansmiley.github.io/p5">
+            Portfolio</a></li>
+          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="https://github.com/briansmiley">
+            Github</a></li>
+          <li className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1"><a href="https://www.linkedin.com/in/brian-smiley-1289a6121/">
+            LinkedIn</a></li>
         </ul>
       </nav>
     </header>)
