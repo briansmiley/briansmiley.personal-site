7b8bad47566df9f3d589ee49dca9b17ad3115e15
Brian Smiley <briantsmiley42@gmail.com>
2024-07-24 18:28:25 -0400

    restyles navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,26 @@
 import { Link } from "@remix-run/react";
 
 type NavBarItemProps = {
-  label: string,
-  url: string,
-  key: string,
-  internal: boolean
-}
-function NavBarLink({label, url, key, internal = true}: NavBarItemProps) {
+  label: string;
+  url: string;
+  key: string;
+  internal: boolean;
+};
+function NavBarLink({ label, url, key, internal = true }: NavBarItemProps) {
   return (
-    <li key={key} className="flex items-center hover:bg-blue-500 w-fit h-full pr-2 pl-1">
-      {internal ? <Link to={url}>{label}</Link> : <a href={url} rel="noopener noreferrer" target="_blank">{label}</a>}
+    <li
+      key={key}
+      className="flex items-center hover:bg-blue-500 rounded-xl  w-fit h-[70%] px-4 py-1 transition-all duration-300"
+    >
+      {internal ? (
+        <Link to={url}>{label}</Link>
+      ) : (
+        <a href={url} rel="noopener noreferrer" target="_blank">
+          {label}
+        </a>
+      )}
     </li>
-  )
+  );
 }
 const links = [
   {
@@ -38,15 +47,18 @@ const links = [
     label: "LinkedIn",
     url: "https://www.linkedin.com/in/brian-smiley-1289a6121/",
     internal: false
-  },
-]
+  }
+];
 export default function Navbar() {
-    return(
-    <header className="h-12 bg-fuchsia-500">
-      <nav className="h-full">
-        <ul className="font-semibold text-blue-200 underline flex justify-start h-full">
-          {links.map((link, idx) => NavBarLink(Object.assign(link, {key: String(idx)})))}
+  return (
+    <header className="h-12 bg-indigo-500">
+      <nav className="h-full flex justify-center ">
+        <ul className="font-semibold text-blue-200 underline flex items-center justify-between gap-2 h-full">
+          {links.map((link, idx) =>
+            NavBarLink(Object.assign(link, { key: String(idx) }))
+          )}
         </ul>
       </nav>
-    </header>)
-    } 
+    </header>
+  );
+}
