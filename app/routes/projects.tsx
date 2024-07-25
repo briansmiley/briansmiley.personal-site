8712a4586aaefe44d01bed6e095615bdbeb6bc2a
Brian Smiley <briantsmiley42@gmail.com>
2024-07-25 14:51:53 -0400

    adds a stupid p5 logo

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -43,13 +43,20 @@ export default function Projects() {
   return (
     <div className="flex flex-col w-full h-full items-center mt-5">
       {/* p5 carousel */}
-      <div className="relative flex flex-col items-center w-[90%] h-[45%] p-4">
+      <div className="relative flex flex-col items-center w-[90%] h-[45%] px-4">
         <div className="h-full w-full absolute inset-0 bg-indigo-300 opacity-50 -z-10 rounded-[25px] sm:rounded-[50px] md:rounded-[100px] transition-all duration-300 hover:scale-110"></div>
-        <h1 className="font-bold text-3xl basis-1/6">
-          <a href="https://p5js.org/" className=" text-red-600 underline">
-            p5.js
-          </a>
-          <span className="text-black"> Sketches</span>
+        <h1 className="flex items-center font-bold text-xl min-[300px]:text-4xl basis-1/6 transition-all duration-300">
+          <div className="flex items-center">
+            <a
+              href="https://p5js.org/"
+              className=" text-red-600 underline flex items-center"
+            >
+              <span>p5</span>
+              <P5Logo className="self-end h-3 w-3 " />
+              <span>js </span>
+            </a>
+            <span className="text-black ml-1">Sketches</span>
+          </div>
         </h1>
         {/* <ProjectCarousel className="" projects={p5Projects} /> */}
         <EmblaProjectCarousel className="basis-4/6" projects={p5Projects} />
@@ -57,3 +64,18 @@ export default function Projects() {
     </div>
   );
 }
+const P5Logo = ({ className }: { className: string }) => {
+  return (
+    <svg
+      viewBox="0 0 28 28"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      className={`animate-spin [animation-duration:5s] ${className}`}
+    >
+      <path
+        d="M16.909,10.259l8.533-2.576l1.676,5.156l-8.498,2.899l5.275,7.48 l-4.447,3.225l-5.553-7.348L8.487,26.25l-4.318-3.289l5.275-7.223L0.88,12.647l1.678-5.16l8.598,2.771V1.364h5.754V10.259z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  );
+};
