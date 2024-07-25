f98a96337696686bed6bd1a5fd97d66fb82de4e1
Brian Smiley <briantsmiley42@gmail.com>
2024-07-24 21:38:15 -0400

    resizes carousel

--- app/components/ProjectCarousel.tsx
+++ app/components/ProjectCarousel.tsx
@@ -24,11 +24,11 @@ export default function ProjectCarousel({
     <Carousel className={`${className} w-[75%] p-2`}>
       <CarouselContent>
         {projects.map((project, idx) => (
-          <CarouselItem className="basis-1/3" key={idx}>
+          <CarouselItem className="basis-3/5" key={idx}>
             <div className="p-1">
               <Link to={project.url}>
                 <Card className="bg-gradient-to-br from-indigo-500 to-indigo-600 border-none text-slate-200 shadow-lg">
-                  <CardHeader className="flex items-center justify-center p-6">
+                  <CardHeader className="flex items-center justify-center py-2 text-xs sm:text-base md:text-lg lg:text-xl transition-all duration-300">
                     <span>
                       <a href={project.url}>{project.title}</a>
                     </span>
