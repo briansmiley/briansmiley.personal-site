92989b790d06e78e72ed06e21b9babf2d7715c24
Brian Smiley <briantsmiley42@gmail.com>
2024-07-25 15:58:36 -0400

    pretier

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,13 +1,13 @@
 export default function About() {
-    return (
-        <div className="mx-5">
-            <h1 className="font-semibold text-4xl">About</h1>
-            <p>
-                Hi I'm Brian
-            </p>
-            <img src="/brian.jpg" className="object-scale-down w-[500px]" alt="Brian"/>
-        </div>
-    )
+  return (
+    <div className="mx-5">
+      <h1 className="font-semibold text-4xl">About</h1>
+      <p>Hi I&apos;m Brian</p>
+      <img
+        src="/brian.jpg"
+        className="object-scale-down w-[500px]"
+        alt="Brian"
+      />
+    </div>
+  );
 }
-
-
