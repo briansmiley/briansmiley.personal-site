871a1f8fd21aaa573b34c6be2a213f5aea015097
Brian Smiley <briantsmiley42@gmail.com>
2024-09-24 14:06:12 -0400

    updates title/favicon

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+  return [{ title: "BinarySmile" }, { name: "description", content: "" }];
 };
-
-
 
 export default function Index() {
   return (
