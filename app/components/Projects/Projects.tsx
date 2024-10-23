28f3bc3031a217700b86737f604163867578a25f
Brian Smiley <briantsmiley42@gmail.com>
2024-10-23 16:13:03 -0400

    restyles web/other labels

--- app/components/Projects/Projects.tsx
+++ app/components/Projects/Projects.tsx
@@ -7,14 +7,12 @@ export default function Projects() {
   return (
     <div className="flex flex-col w-full h-full items-center py-5 overflow-y-auto">
       <div className="flex flex-col gap-5 px-2 md:px-10">
-        <span className="flex flex-col gap-1">
-          <span className=" text-xl md:text-2xl underline font-semibold">
-            Web
-          </span>
+        <span className="flex flex-col">
+          <span className=" text-xl md:text-2xl  font-semibold px-2">Web</span>
           <ProjectList projects={projects} />
         </span>
-        <span className="flex flex-col gap-1">
-          <span className=" text-xl md:text-2xl underline font-semibold">
+        <span className="flex flex-col">
+          <span className=" text-xl md:text-2xl  font-semibold px-2">
             Other
           </span>
           <ProjectList projects={offProjects} />
