fefd7b0e2c084c35e2ffed08a637a5298bb4a330
Brian Smiley <briantsmiley42@gmail.com>
2024-11-04 17:18:08 -0500

    restyles info popover email/resume links

--- app/components/BioHeader/Info.tsx
+++ app/components/BioHeader/Info.tsx
@@ -2,34 +2,41 @@ import { Link } from "@remix-run/react";
 import { CopyIcon, MailIcon } from "lucide-react";
 import FrameworkIcon from "../FrameworkIcon/FrameworkIcon";
 import { useToast } from "~/hooks/use-toast";
+import { Button } from "../ui/button";
 
 export default function Info() {
   const { toast } = useToast();
   return (
     <div className="flex flex-col items-start justify-center w-full">
       <Link
-        className="flex items-center justify-start"
+        className="flex items-center justify-start hover:underline"
         to="/resume.pdf"
         target="_blank"
         rel="noopener noreferrer"
       >
         Resumé <FrameworkIcon icon="pdf" />
       </Link>
-      <div className="flex items-center justify-start gap-4 w-full">
-        <span className="flex items-center gap-1">
-          <MailIcon className="w-4 h-4" /> briansmiley@proton.me{" "}
-        </span>
-        <button
-          className="flex items-center justify-start"
+      <div className="flex items-center justify-start gap-2 w-full">
+        <a
+          href="mailto:briansmiley@proton.me"
+          className="flex items-center gap-1 hover:underline"
+        >
+          <MailIcon className="w-4 h-4" /> briansmiley@proton.me
+        </a>
+        <Button
+          className="flex items-center justify-center"
+          title="Copy"
           onClick={() => {
             navigator.clipboard.writeText("briansmiley@proton.me");
             toast({
               title: "Copied email"
             });
           }}
+          variant="ghost"
+          size="icon"
         >
-          <CopyIcon className="w-4 h-4" />
-        </button>
+          <CopyIcon className="" />
+        </Button>
       </div>
     </div>
   );
